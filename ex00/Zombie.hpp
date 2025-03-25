de0e309079ade593df276893f5ef1000b0735d3e
Vladislav Sanin <vladislav.sanin.419@gmail.com>
2025-03-25 22:43:27 +0100

    started 00

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsanin <vsanin@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 21:24:50 by vsanin            #+#    #+#             */
+/*   Updated: 2025/03/25 21:27:49 by vsanin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		void announce(void);
+};
+
+#endif
